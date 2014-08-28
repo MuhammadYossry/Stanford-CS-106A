62bf9f5b25773461ac237c26d5cd9eea1614f09e
Muhammad Yossry <mohamedyosry3000@gmail.com>
2014-08-28 11:23:53 +0300

    assignment1 - problem2

--- Assignment1/StoneMasonKarel.java
+++ Assignment1/StoneMasonKarel.java
@@ -13,5 +13,40 @@ import stanford.karel.*;
 public class StoneMasonKarel extends SuperKarel {
 
 	// You fill in this part
-
+	public void run() {
+		repairDamage();
+	}
+	private void repairDamage() {
+		while(true){
+			repairColoumn();
+			if(facingSouth() && leftIsBlocked()){
+				break;
+			}
+			moveToNextColoumn();
+		}
+		turnLeft();
+	}
+	private void repairColoumn() {
+		if(rightIsBlocked()){
+			turnLeft();
+		}else {
+			turnRight();
+		}
+		while(frontIsClear()){
+			if(noBeepersPresent()){
+				putBeeper();
+			}
+			move();
+		}
+	}
+	private void moveToNextColoumn() {
+		if(facingNorth()){
+			turnRight();
+		}else {
+			turnLeft();
+		}
+		for(int i = 0; i < 4; i++){
+			move();
+		}
+	}
 }
