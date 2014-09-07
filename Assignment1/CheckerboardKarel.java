a075306ae361d9a23e81a9e3875dff9ea0810c70
Muhammad Yossry <mohamedyosry3000@gmail.com>
2014-09-07 13:25:41 +0300

    assignment1 - problem3

--- Assignment1/CheckerboardKarel.java
+++ Assignment1/CheckerboardKarel.java
@@ -12,5 +12,68 @@ import stanford.karel.*;
 public class CheckerboardKarel extends SuperKarel {
 
 	// You fill in this part
+	//fill even blocks
+	//turn left
+	// put beeper
+	// while(fornIsClear)
+	// move
+	// if(FrontIsClear)
+	//  move
+	// putBeeper
+	public void run() { 
+		 if(leftIsBlocked()){
+			 fillOneRow();
+		 }else{
+			 turnLeft();
+		     fillColumn();
+		 }
+		}
+	
+	private void fillColumn() {
+		if(beepersPresent() && frontIsClear()){
+			move();
+		}
+		while(frontIsClear()){
+			putBeeper();
+			if(frontIsClear()){
+			  move();
+			}
+			if(frontIsClear()){
+				move();
+				if(frontIsBlocked() && noBeepersPresent()){
+					putBeeper();
+					switchColumn();
+					if(frontIsClear()){
+					  move();
+					}
+				}
+			}else{
+				switchColumn();
+			}
+		}
+	}
+	
+	private void switchColumn(){
+		if(facingNorth() && rightIsClear()){
+			turnRight();
+			move();
+			turnRight();
+		}else if(facingSouth() && leftIsClear()) {
+			turnLeft();
+			move();
+			turnLeft();
+		}
+	}
+	
+	private void fillOneRow(){
+		putBeeper();
+		while(frontIsClear()){
+			move();
+			if(frontIsClear()){
+				move();
+				putBeeper();
+			}
+		}
+	}
 
 }
