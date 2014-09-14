560cce01ec7b5fbe38816fb879bc5448da068e1c
Muhammad Yossry <mohamedyosry3000@gmail.com>
2014-09-14 12:33:32 +0300

    assignment2 - problem 5

--- Assignment2/FindRange.java
+++ Assignment2/FindRange.java
@@ -9,8 +9,28 @@
 import acm.program.*;
 
 public class FindRange extends ConsoleProgram {
+	private static final int SENTINEL = 0;
 	public void run() {
-		/* You fill this in */
-	}
+		int input = 0;
+		int min =0;
+		int max = 0;
+		println("This program finds the largest and smallest numbers.");
+		while(true) {
+			input = readInt("?");
+			if(input != SENTINEL){
+				min = input < min ? input : min;
+				max = input > max ? input : max;
+			}else {
+				break;
+			}
+		}
+		
+		if(max == 0 && min == 0){
+			println("You entered no values");
+		} else {
+			if( min == 0) min = max; 
+			if (max == 0) max = min;
+			println("The maximum is " + max + " And the minimum is " + min + ".");
+		}
 }
-
+}
