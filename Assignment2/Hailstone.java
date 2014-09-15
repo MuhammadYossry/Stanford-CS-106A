20e22923dbb3700052c74cd32a307f3076f7c3ea
Muhammad Yossry <mohamedyosry3000@gmail.com>
2014-09-15 12:33:52 +0300

    assignment2 - problem 6

--- Assignment2/Hailstone.java
+++ Assignment2/Hailstone.java
@@ -10,7 +10,16 @@ import acm.program.*;
 
 public class Hailstone extends ConsoleProgram {
 	public void run() {
-		/* You fill this in */
+		int n = readInt("Pick some positive integer: ");
+		while( n != 1){
+			if(n % 2 == 0){
+				println(n + " is even, so I take half: " + (n / 2));
+				n /= 2;
+			}else {
+				println(n + " is odd, so I make 3n + 1: " + (n*3 + 1));
+				n = n*3 + 1;
+			}
+		}
 	}
 }
 
